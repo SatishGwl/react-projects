63dc3d36991ccc70d699009b81f2f9c519294f35
SatishGwl <satish.srcem@gmail.com>
2021-07-19 22:35:10 +0530

    birthday remeber

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import List from './List'
+import data from './data';
 
 function App() {
+  const[people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <div className="container">
+      <h3>{people.length} birthdays todays</h3>
+      <List people = {people}/>
+      <button onClick={() => setPeople([])}>clear all</button>
+
     </div>
+    </main>
   );
 }
 
